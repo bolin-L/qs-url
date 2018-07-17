d949f65f2b11083fc1759566cb4ecd17aee2f864
liaobolin <liaobolin@u51.com>
2018-07-17 20:07:54 +0800

    :art: query 与 params操作方法已写好

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import qs from 'qs';
+import query from './lib/query';
+import params from './lib/params';
 import baseUrl from 'baseUrl';
 
 
 export default {
     ...qs,
-    setBaseUrl() {
-
+    ...query,
+    ...params,
+    setBaseUrl(url) {
+        baseUrl = url;
     }
 };
