0cee1ec382907df9b0fa5511a78a17f3db9dcd75
liaobolin <liaobolin@u51.com>
2018-07-18 20:49:20 +0800

    :art: add removeQuery test

--- test/query.js
+++ test/query.js
@@ -85,27 +85,89 @@ describe('Query', () => {
         const BASE_URL = 'http://www.bolin.site';
 
         it(`should be return ${BASE_URL}?id=2 when input {id: 2}`, () => {
-            const url = query.addQuery({id: 2}, BASE_URL);
+            const url = query.replaceQuery({id: 2}, BASE_URL);
 
             expect(url).to.equal(`${BASE_URL}?id=2`);
         });
 
-        it(`should be return ${BASE_URL}?id=2&status=success when input {id: 2, status: 'success'}`, () => {
-            const url = query.addQuery({id: 2, status: 'success'}, BASE_URL);
+        it(`should be return ${BASE_URL}?id=1 when input {id: 1}`, () => {
+            const url = query.replaceQuery({id: 1}, `${BASE_URL}?id=2`);
 
-            expect(url).to.equal(`${BASE_URL}?id=2&status=success`);
+            expect(url).to.equal(`${BASE_URL}?id=1`);
         });
 
-        it(`should be return ${BASE_URL}?id=2&status=success when input {status: 'success'}`, () => {
-            const url = query.addQuery({status: 'success'}, `${BASE_URL}?id=2`);
+        it(`should be return ${BASE_URL}?id=1&status=success when input {id: 2, status: 'success'}`, () => {
+            const url = query.replaceQuery({id: 1, status: 'success'}, `${BASE_URL}?id=2`);
 
-            expect(url).to.equal(`${BASE_URL}?id=2&status=success`);
+            expect(url).to.equal(`${BASE_URL}?id=1&status=success`);
         });
 
         it(`should be return ${BASE_URL}?id=2&status=success#/user/info?name=lbl when input {id: 2, status: 'success'}`, () => {
-            const url = query.addQuery({id: 2, status: 'success'}, `${BASE_URL}#/user/info?name=lbl`);
+            const url = query.replaceQuery({id: 2, status: 'success'}, `${BASE_URL}#/user/info?name=lbl`);
 
             expect(url).to.equal(`${BASE_URL}?id=2&status=success#/user/info?name=lbl`);
         });
-    })
+
+        it(`should be return ${BASE_URL}?id=1&status=success#/user/info?name=lbl when input {id: 1}`, () => {
+            const url = query.replaceQuery({id: 1}, `${BASE_URL}?status=success#/user/info?name=lbl`);
+
+            expect(url).to.equal(`${BASE_URL}?status=success&id=1#/user/info?name=lbl`);
+        });
+    });
+
+    describe('#removeQuery', () => {
+        const BASE_URL = 'http://www.bolin.site';
+
+        it(`should be return ${BASE_URL} when input null`, () => {
+            const url = query.removeQuery(null, BASE_URL);
+
+            expect(url).to.equal(`${BASE_URL}`);
+        });
+
+        it(`should be return ${BASE_URL} when input null`, () => {
+            const url = query.removeQuery(null, `${BASE_URL}?id=1&status=success`);
+
+            expect(url).to.equal(`${BASE_URL}`);
+        });
+
+        it(`should be return ${BASE_URL} when input id`, () => {
+            const url = query.removeQuery('id', `${BASE_URL}?id=1`);
+
+            expect(url).to.equal(`${BASE_URL}`);
+        });
+
+        it(`should be return ${BASE_URL} when input ['id']`, () => {
+            const url = query.removeQuery(['id'], `${BASE_URL}?id=1`);
+
+            expect(url).to.equal(`${BASE_URL}`);
+        });
+
+        it(`should be return ${BASE_URL} when input ['id', 'status']`, () => {
+            const url = query.removeQuery(['id', 'status'], `${BASE_URL}?id=1&status=success`);
+
+            expect(url).to.equal(`${BASE_URL}`);
+        });
+
+        it(`should be return ${BASE_URL}?id=1#/user/info?name=lbl when input 'status'`, () => {
+            const url = query.removeQuery('status', `${BASE_URL}?id=1&=status=success#/user/info?name=lbl`);
+
+            expect(url).to.equal(`${BASE_URL}?id=1#/user/info?name=lbl`);
+        });
+
+        it(`should be return ${BASE_URL}#/user/info?name=lbl when input ['id', 'status']`, () => {
+            const url = query.removeQuery(['id', 'status'], `${BASE_URL}?id=1&status=success#/user/info?name=lbl`);
+
+            expect(url).to.equal(`${BASE_URL}#/user/info?name=lbl`);
+        });
+    });
+
+    describe('#getPath', () => {
+        const BASE_URL = 'http://www.bolin.site?id=1&status=success#/user/info?name=lbl';
+
+        it(`should be return ${BASE_URL}`, () => {
+            const url = query.getPath('', BASE_URL);
+
+            expect(url).to.equal(`http://www.bolin.site`);
+        });
+    });
 });
